6de6eba32ea34fd561c0382447251ab9180a86aa
Quang Mai <quangmai@kms-technology.com>
2021-10-05 01:03:44 +0700

    Move classes to seperated file

--- index.js
+++ index.js
@@ -1,130 +1,4 @@
-class TrieNode {
-  constructor() {
-    this.ipAddress = undefined;
-    this.count = 0;
-    this.children = {};
-  }
-}
-
-const TOP_NUMBER = 100;
-class RequestHandler {
-  constructor(topNumber) {
-    this.root = new TrieNode();
-    this.topCounts = [];
-    this.topIpAddresses = [];
-    this.topNumber = topNumber || TOP_NUMBER;
-  }
-
-  request_handled(ipAddress) {
-    if (ipAddress.length === 0) return;
-    let character;
-    let currentNode = this.root;
-    for (let i = 0; i < ipAddress.length; i++) {
-      character = ipAddress[i];
-      if (!currentNode.children[character]) {
-        currentNode.children[character] = new TrieNode();
-      }
-      currentNode = currentNode.children[character];
-    }
-
-    currentNode.ipAddress = ipAddress;
-    currentNode.count = currentNode.count + 1;
-
-    try {
-      buildTopCounts(
-        this.topCounts,
-        this.topIpAddresses,
-        currentNode,
-        this.topNumber
-      );
-    } catch (error) {
-      console.log('request_handled', error);
-    }
-  }
-
-  top100() {
-    if (!this.topIpAddresses || this.topIpAddresses.length === 0) {
-      this.topIpAddresses = convertToIpAddresses(this.topCounts);
-    }
-
-    return this.topIpAddresses;
-  }
-
-  clear() {
-    delete this.root.children;
-    this.root = new TrieNode();
-    this.top100 = [];
-  }
-}
-
-function buildTopCounts(topCounts, topIpAddresses, node, topNumber) {
-  if (!topCounts || !topIpAddresses || !node || !topNumber) {
-    throw 'Empty input values.';
-  }
-
-  if (topCounts.length === 0) {
-    topCounts.push(node);
-    return;
-  }
-
-  const existIndex = topCounts.indexOf(node);
-  if (existIndex > -1) {
-    const previousIndex = existIndex - 1;
-    if (previousIndex >= 0 && topCounts[previousIndex].count < node.count) {
-      const moved = moveUpCount(topCounts, existIndex);
-      if (moved) {
-        topIpAddresses = [];
-      }
-    }
-  } else if (topCounts.length < topNumber - 1) {
-    topCounts.push(node);
-    topIpAddresses = [];
-  } else {
-    const last = topCounts[topCounts.length - 1];
-    if (last.count < node.count) {
-      topCounts[topCounts.length - 1] = node;
-      const moved = moveUpCount(topCounts, topCounts.length - 1);
-      if (moved) {
-        topIpAddresses = [];
-      }
-    }
-  }
-}
-
-function convertToIpAddresses(nodes) {
-  let ipAddresses = [];
-
-  if (nodes) {
-    ipAddresses = nodes.map((node) => {
-      return node.ipAddress;
-    });
-  }
-
-  return ipAddresses;
-}
-
-function moveUpCount(arr, needToUpIndex) {
-  let moved = false;
-  let previousIndex = needToUpIndex - 1;
-
-  while (
-    previousIndex >= 0 &&
-    arr[needToUpIndex].count > arr[previousIndex].count
-  ) {
-    swapNodes(arr, needToUpIndex, previousIndex);
-    needToUpIndex = previousIndex;
-    previousIndex = previousIndex - 1;
-    moved = true;
-  }
-
-  return moved;
-}
-
-function swapNodes(arr, i, j) {
-  var tmp = arr[i];
-  arr[i] = arr[j];
-  arr[j] = tmp;
-}
+const RequestHistory = require('./RequestHistory');
 
 const requestIps = [
   '245.87.2.109',
@@ -379,12 +253,12 @@ const requestIps = [
   '185.220.101.249',
 ];
 
-let t = new RequestHandler();
+let requestHistory = new RequestHistory();
 
 for (i = 0; i < requestIps.length; i++) {
-  t.request_handled(requestIps[i]);
+  requestHistory.request_handled(requestIps[i]);
 }
 
-console.log('top10', t.top100());
-console.log('top10', t.topCounts);
-// console.dir(t.root, { depth: null });
+console.log('top10', requestHistory.top100());
+console.log('top10', requestHistory.topCounts);
+// console.dir(requestHistory.root, { depth: null });
